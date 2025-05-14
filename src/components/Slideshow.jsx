bad0580d2be170d6200951c86938d8e1591ee0cc
Hamza Tariq <140837284+Pulsefire-Hamza@users.noreply.github.com>
2025-05-15 01:15:00 +0500

    Update Slideshow.jsx

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Pagination, Autoplay } from "swiper/modules";
-import { Link } from "react-router-dom"; // ← Add this
+import { Link } from "react-router-dom";
 import "swiper/css";
 import "swiper/css/pagination";
 
@@ -30,7 +30,7 @@ const Slideshow = () => {
       src: "https://dataofclient.nyc3.cdn.digitaloceanspaces.com/DULHAHOUSE/WhatsApp%20Image%202025-05-13%20at%204.10.28%20PM%20(2).jpeg",
       link: "/product/8",
     },
-     {
+    {
       src: "https://dataofclient.nyc3.cdn.digitaloceanspaces.com/DULHAHOUSE/WhatsApp%20Image%202025-05-13%20at%204.10.29%20PM.jpeg",
       link: "/product/103",
     },
@@ -78,7 +78,7 @@ const Slideshow = () => {
   const slidesToShow = isMobile ? mobileSlides : desktopSlides;
 
   return (
-<div className="mt-28 md:mt-36 w-full">  
+    <div className="mt-28 md:mt-36 w-full flex justify-center">
       <Swiper
         modules={[Pagination, Autoplay]}
         spaceBetween={0}
@@ -89,21 +89,21 @@ const Slideshow = () => {
       >
         {slidesToShow.map((slide, index) => (
           <SwiperSlide key={index}>
-          <div className="relative w-full flex justify-center items-center 
-  min-h-[200px] sm:min-h-[300px] md:min-h-[350px] lg:min-h-[400px] xl:min-h-[700px]">
-  <Link to={slide.link}>
-  <img
-  src={slide.src}
-  alt={`Slide ${index + 1}`}
-  className="w-full h-full object-cover rounded-xl shadow-lg 
-             lg:w-[851px] lg:h-[316px] 
-             xl:w-[1880px] xl:h-[700px]"
-  loading="lazy"
-/>
-
-  </Link>
-</div>
-
+            <div className="relative w-full flex justify-center items-center 
+              min-h-[200px] sm:min-h-[300px] md:min-h-[350px] 
+              lg:min-h-[316px] lg:w-[851px] 
+              xl:min-h-[700px] xl:w-[1880px]">
+              <Link to={slide.link}>
+                <img
+                  src={slide.src}
+                  alt={`Slide ${index + 1}`}
+                  className="w-full h-full object-cover rounded-xl shadow-lg 
+                    lg:w-[851px] lg:h-[316px] 
+                    xl:w-[1880px] xl:h-[700px]"
+                  loading="lazy"
+                />
+              </Link>
+            </div>
           </SwiperSlide>
         ))}
       </Swiper>
